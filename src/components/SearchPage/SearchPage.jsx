a5d12182caa104034e24e4212cd55863cef862bf
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-06-26 13:26:14 -0500

    created and imported Search page and team builder page

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import ('./TeamBuilder.css')
+import ('./SearchPage.css')
 
 // This is one of our simplest components
 // It doesn't have local state
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is
 
-function TeamBuilder() {
+function SearchPage() {
   return (
     <div className="container">
-      <p>TeamBuilder page </p>
+      <p>Search Page </p>
     </div>
   );
 }
 
-export default TeamBuilder;
+export default SearchPage;
