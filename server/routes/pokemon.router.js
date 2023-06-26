1bc1d1e9ba90e8f00f33451af094edd286b11532
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-06-26 12:12:47 -0500

    started router, reducer

--- server/routes/pokemon.router.js
+++ server/routes/pokemon.router.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
+const axios = require("axios");
 
-router.get('/', (req, res) => {
-    // GET route code here
-  });
-  
-  /**
-   * POST route template
-   */
-  router.post('/', (req, res) => {
-    // POST route code here
-  });
+router.get("/", (req, res) => {
+  axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then((response) => {
+      console.log("app axios get is working", response);
+      res.send(response.data);
 
-  module.exports = router;
+      // GET route code here
+    })
+    .catch((error) => {
+      console.log("error in pokemon router", error);
+    });
+});
+
+/**
+ * POST route template
+ */
+router.post("/", (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const pokemonRouter = require('./routes/pokemon.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,7 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-// app.use('/api/pokemon', pokemonRouter);
+app.use('/api/pokemon', pokemonRouter);
 
 // Serve static files
 app.use(express.static('build'));
